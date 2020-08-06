3c9e14701086f1c4c2ef40973c53f4d9e46d2f71
EdilsonFilho <edilsonfilhosh@gmail.com>
2020-08-06 17:42:05 -0300

    print do radionbutton

--- controle.py
+++ controle.py
@@ -2,7 +2,20 @@ from PyQt5 import QtWidgets, uic
 import sys
 
 def funcao_principal():
-    print("teste")
+    linha1 = formulario.lineEdit.text()
+    linha2 = formulario.lineEdit_2.text()
+    linha3 = formulario.lineEdit_3.text()
+    
+    print("Codigo:",linha1)
+    print("Descricao",linha2)
+    print("Preço:",linha3)
+
+    if formulario.radioButton.isChecked():
+        print("Categoria ceral")
+    elif formulario.radioButton_2.isChecked():
+        print("Categoria Grão ")
+    else:             
+        print("Categoria Pó")
 
 
 app=QtWidgets.QApplication([])
